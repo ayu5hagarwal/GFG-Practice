cf26557580f29bc80470ed4c2aa5d452d8a6506d
Ayush Agarwal <116223165+ayu5hagarwal@users.noreply.github.com>
2024-01-09 21:55:52 +0530

    Updated Solution - GfG to GitHub

--- Easy/Add two numbers represented by two arrays/add-two-numbers-represented-by-two-arrays.cpp
+++ Easy/Add two numbers represented by two arrays/add-two-numbers-represented-by-two-arrays.cpp
@@ -9,36 +9,35 @@ class Solution{
     public:
     string calc_Sum(int *a,int n,int *b,int m){
     // Complete the function
-    int carry = 0;
+    int carry = 0;  
     string ans;
     int i = n-1;
     int j = m-1;
     while(i>=0 && j>=0){
         int x = a[i] + b[j] + carry;
         int digit = x%10;
-        ans.push_back(digit+'0');
+        ans.push_back(digit + '0');
+        carry = x/10;
+        i--,j--;
+    }
+     while(i>=0){
+        int x = a[i] + 0 + carry;
+        int digit = x%10;
+        ans.push_back(digit + '0');
         carry = x/10;
         i--;
-        j--;
     }
-    while(i>=0){
-        int x = a[i] + carry;
+     while(j>=0){
+        int x =  b[j] + 0 + carry;
         int digit = x%10;
-        ans.push_back(digit+'0');
-        carry = x/10;
-        i--;
-    }
-    while(j>=0){
-        int x = b[j] + carry;
-        int digit = x%10;
-        ans.push_back(digit+'0');
+        ans.push_back(digit + '0');
         carry = x/10;
         j--;
     }
     if(carry){
-        ans.push_back(carry + '0'); 
+        ans.push_back(carry + '0');
     }
-    while(ans[ans.size()-1] == '0'){
+    while(ans[ans.size()-1]=='0'){
         ans.pop_back();
     }
     reverse(ans.begin(),ans.end());
